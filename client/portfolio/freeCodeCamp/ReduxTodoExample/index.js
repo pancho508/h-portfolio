92be949bb5462d831933fbf1e2de2bd220632a9d
pancho1337 <pancho404x@gmail.com>
2020-09-21 04:25:48 -0500

    randomQuoteMachine done

---
+++ client/portfolio/freeCodeCamp/ReduxTodoExample/index.js
@@ -0,0 +1,105 @@
+//Redux - Una accion para usar en reducer
+const ADD = 'ADD';
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message: message
+  }
+};
+//Redux - Nuestro Reducer que toma la accion y nos da un nuevo estado para la Store
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+//Redux - Creamos nuestra store usando el reducer * Aqui tambien podemos usar combineReducers EJ.
+// const { combineReducers } = Redux;
+// const todoApp = combineReducers({
+// 	todos,
+// 	visibilityFilter
+// });
+const store = Redux.createStore(messageReducer);
+
+// ReactRedux - Provider nos deja encapsular al app dandole aceso a nuestra store y connect nos deja crear el contenedor de nuestro componente principal 
+const Provider = ReactRedux.Provider;
+const connect = ReactRedux.connect;
+
+// Change code below this line
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {
+    this.setState((state) => ({
+      input: '',
+      messages: state.messages.concat(state.input)
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <h2>Create a new Quote:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange} /><br />
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map((message, idx) => {
+            return (
+              <li key={idx}>{message}</li>
+            )
+          })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
+// Change code above this line
+
+const mapStateToProps = (state) => {
+  return { messages: state }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message))
+    }
+  }
+};
+//ReactRedux - Creamos unuestro contenedor usando mapStateToProps(Conecta el estado de la store de redux hacia los props del componente) y mapDispatchToProps(Conecta ): It connects redux actions to react props.
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+//React - Encapsulamos nuestro contendor para dar acceso hacia nuestra store
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Container />
+      </Provider>
+    );
+  }
+};
+
+//ReactDom - Reproducimos el tronco  
+ReactDOM.render(
+  <AppWrapper />,
+  document.getElementById('quote-box')
+);
--- client/portfolio/freeCodeCamp/randomQuoteMachine/index.js
+++ client/portfolio/freeCodeCamp/randomQuoteMachine/index.js
@@ -1,105 +1,168 @@
-//Redux - Una accion para usar en reducer
 const ADD = 'ADD';
+const SET = 'SET';
 const addMessage = (message) => {
   return {
     type: ADD,
     message: message
   }
 };
-//Redux - Nuestro Reducer que toma la accion y nos da un nuevo estado para la Store
-const messageReducer = (state = [], action) => {
+const setMessages = (messages) => {
+  return {
+    type: SET,
+    messages: messages
+  }
+};
+const messageReducer = (state = {}, action) => {
   switch (action.type) {
     case ADD:
-      return [
+      return {
         ...state,
-        action.message
-      ];
+        messages: [action.message, ...state.messages]
+      }
+    case SET:
+      return {
+        ...state,
+        messages: action.messages
+      }
     default:
       return state;
   }
 };
-//Redux - Creamos nuestra store usando el reducer * Aqui tambien podemos usar combineReducers EJ.
-// const { combineReducers } = Redux;
-// const todoApp = combineReducers({
-// 	todos,
-// 	visibilityFilter
-// });
 const store = Redux.createStore(messageReducer);
-
-// ReactRedux - Provider nos deja encapsular al app dandole aceso a nuestra store y connect nos deja crear el contenedor de nuestro componente principal 
 const Provider = ReactRedux.Provider;
 const connect = ReactRedux.connect;
 
-// Change code below this line
 class Presentational extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: '',
-      messages: []
+      author: '',
+      quote: '',
+      showMessage: {
+        author: "Everything that irritates us about others can lead us to an understanding of ourselvese",
+        quote: "C.G. Jung "
+      },
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.submitMessage = this.submitMessage.bind(this);
+  }
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+      .then(res => res.json())
+      .then(data => this.props.setNewMessages(data.quotes))
+      .then(() => this.randomQuoteClick())
+      .then(() => {
+        console.log("This project is working with React/Redux, soo much fun xD")
+      })
+  }
+  randomQuoteClick(e) {
+    let idx = Math.floor(Math.random() * this.props.messages.length)
+    this.setState({
+      showMessage: this.props.messages[idx]
+    })
   }
   handleChange(event) {
     this.setState({
-      input: event.target.value
+      [event.target.name]: event.target.value
     });
   }
   submitMessage() {
-    this.setState((state) => ({
-      input: '',
-      messages: state.messages.concat(state.input)
-    }));
+    this.props.submitNewMessage({ author: this.state.author, quote: this.state.quote })
+    this.setState({
+      author: '',
+      quote: ''
+    });
+    alert('Success, Your quote was added!')
   }
   render() {
+    var asciiStyle = {
+      display: "inline-block",
+      letterSpacing: "-0.2em",
+      lineHeight: "1em",
+      textAlign: 'left',
+      fontSize: '13px'
+    }
+    var inputASCII = [
+      `<pre><code>+-------+ +-------+    +-------+ +--------+   +-------+ +-------+    +--------+ +-------+    +-------+ +------+`,
+      `                             _               ______          _     ______                        _            `,
+      `                            | |             (______)        | |   (_____ \\                      | |           `,
+      ` ____   _____  ____    ____ | |__    ___     _     _  _____ | |    _____) ) _____  ____    ____ | |__    ___  `,
+      `|  _ \\ (____ ||  _ \\  / ___)|  _ \\  / _ \\   | |   | || ___ || |   |  __  / (____ ||  _ \\  / ___)|  _ \\  / _ \\ `,
+      `| |_| |/ ___ || | | |( (___ | | | || |_| |  | |__/ / | ____|| |   | |  \\ \\ / ___ || | | |( (___ | | | || |_| |`,
+      `|  __/ \\_____||_| |_| \\____)|_| |_| \\___/   |_____/  |_____) \\_)  |_|   |_|\\_____||_| |_| \\____)|_| |_| \\___/ `,
+      `|_|                                                                                                           `,
+      "+-------+ +-------+    +-------+ +--------+   +-------+ +-------+    +--------+ +-------+    +-------+ +------+",
+      "</code></pre>",
+    ].join('\n')
+
+    // dangerouslySetInnerHTML expects an object like this:
+    var wrappedASCII = { __html: inputASCII };
     return (
-      <div>
-        <h2>Create a new Quote:</h2>
-        <input
-          value={this.state.input}
-          onChange={this.handleChange} /><br />
-        <button onClick={this.submitMessage}>Submit</button>
-        <ul>
-          {this.state.messages.map((message, idx) => {
-            return (
-              <li key={idx}>{message}</li>
-            )
-          })
-          }
-        </ul>
+      <div id={"quote-box"} >
+        <div>
+          <span id={"asciiStyle"} dangerouslySetInnerHTML={wrappedASCII}></span>
+          <p>Quote of the Day!!</p>
+        </div>
+        <div>
+          <div id={"text"}>
+            <div className={"box"}>
+              <p className={"glitch"} value={this.state.showMessage.quote}>{this.state.showMessage.quote}</p>
+            </div>
+            <div className={"box"}>
+              <h5 className={"glitch"} id={'author'} value={this.state.showMessage.author}>{this.state.showMessage.author}</h5>
+            </div>
+            <div className={"box"}>
+              <a className="button" id="tweet-quote" title="Tweet this quote!" href="https://twitter.com/intent/tweet" target="_blank">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+            <button id={'new-quote'} onClick={this.randomQuoteClick.bind(this)}>Random Quote</button>
+          </div>
+        </div>
+
+        <div>
+          <h3>Summit Your Quote:</h3>
+          <input
+            name={"author"}
+            placeholder={'Your Name'}
+            value={this.state.author}
+            onChange={this.handleChange.bind(this)} />
+          <br />
+          <input
+            name={"quote"}
+            placeholder={'Your Quote'}
+            value={this.state.quote}
+            onChange={this.handleChange.bind(this)} />
+          <br />
+          <button id={"submitQuote"} onClick={this.submitMessage.bind(this)}>Submit</button>
+        </div>
+
       </div>
     );
   }
 };
-// Change code above this line
-
 const mapStateToProps = (state) => {
-  return { messages: state }
+  return state
 };
-
 const mapDispatchToProps = (dispatch) => {
   return {
     submitNewMessage: (message) => {
       dispatch(addMessage(message))
+    },
+    setNewMessages: (messages) => {
+      dispatch(setMessages(messages))
     }
   }
 };
-//ReactRedux - Creamos unuestro contenedor usando mapStateToProps(Conecta el estado de la store de redux hacia los props del componente) y mapDispatchToProps(Conecta ): It connects redux actions to react props.
 const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
-//React - Encapsulamos nuestro contendor para dar acceso hacia nuestra store
 class AppWrapper extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Container id="quote-box" />
+        <Container />
       </Provider>
     );
   }
 };
-
-//ReactDom - Reproducimos el tronco  
 ReactDOM.render(
   <AppWrapper />,
-  document.getElementById('quote-box')
+  document.getElementById('app')
 );
