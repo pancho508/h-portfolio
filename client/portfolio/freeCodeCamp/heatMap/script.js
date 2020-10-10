d7d560a1bb48c364b377d418396d9ed49064a4c5
pancho1337 <pancho404x@gmail.com>
2020-10-09 23:51:10 -0500

    heatMap Ganesh step 10

--- client/portfolio/freeCodeCamp/heatMap/script.js
+++ client/portfolio/freeCodeCamp/heatMap/script.js
@@ -4,6 +4,8 @@ let baseTemp
 let values
 let xScale
 let yScale
+let minYear
+let maxYear
 let width = 1200
 let height = 600
 let padding = 60
@@ -13,15 +15,57 @@ canvas.attr('width', width)
 canvas.attr('height', height)
 
 let generateScales = () => {
+    minYear = d3.min(values, (el) => el['year'])
+    maxYear = d3.max(values, (el) => el['year'])
+    xScale = d3.scaleLinear()
+        .domain([minYear, maxYear + 1])
+        .range([padding, width - padding])
+    yScale = d3.scaleTime()
+        .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)])
+        .range([padding, height - padding])
 
 }
 
 let drawCells = () => {
-
+    canvas.selectAll('rect')
+        .data(values)
+        .enter()
+        .append('rect')
+        .attr('class', 'cell')
+        .attr('fill', (el) => {
+            if (el['variance'] < -1) {
+                return 'SteelBlue'
+            } else if (el['variance'] < 0) {
+                return 'LightSteelBlue'
+            } else if (el['variance'] < 1) {
+                return 'Orange'
+            } else {
+                return 'Red'
+            }
+        })
+        .attr('data-year', (el) => el['year'])
+        .attr('data-month', (el) => el['month'] - 1)
+        .attr('data-temp', (el) => baseTemp + el['variance'])
+        .attr('height', (height - (2 * padding)) / 12)
+        .attr('y', (el) => yScale(new Date(0, el['month'] - 1, 0, 0, 0, 0, 0)))
+        .attr('width', (el) => {
+            return (width - 2 * padding) / (maxYear - minYear)
+        })
+        .attr('x', (el) => xScale(el['year']))
 }
 
 let drawAxes = () => {
-
+    let xAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.format('d'))
+    let yAxis = d3.axisLeft(yScale)
+    canvas.append('g')
+        .call(xAxis)
+        .attr('id', 'x-axis')
+        .attr('transform', 'translate(0,' + (height - padding) + ")")
+    canvas.append('g')
+        .call(yAxis)
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(' + padding + ", 0)")
 }
 
 req.open('GET', url, true)
@@ -31,6 +75,10 @@ req.onload = () => {
     values = obj['monthlyVariance']
     console.log('baseTemp', baseTemp)
     console.log("values", values)
+    generateScales()
+    drawCells()
+    drawAxes()
+
 
 }
 req.send()
